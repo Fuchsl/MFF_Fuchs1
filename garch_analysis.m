7eba9e735a7ff6ac3b9f0c44880fd22ffdf6caa8
Fuchsl <robertfuchs@online.de>
2015-10-13 16:33:34 +0200

    garch_analysis.m

--- garch_analysis.m
+++ garch_analysis.m
@@ -1,1 +1,16 @@
-%%Test1
+%%Aufgabe 1
+
+%Teil a)
+
+% Ticker Symbolder DB auf Yahoo
+tickerSymbs = {'DBK.DE'};     
+
+%% Beginn und Ende der Daten
+dateBeg = '01012000';   
+dateEnd = '01012015';
+
+%% Daten durch die Funktion getPrices laden
+DBData = getPrices(dateBeg, dateEnd, tickerSymbs);
+
+%Teil b)
+
